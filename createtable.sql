baa46f72411a0e57336ef06ca0553ba039e3d7d0
Jun Xia <xiaj8@uci.edu>
2024-04-07 13:57:11 -0700

    Added createtable.sql

---
+++ createtable.sql
@@ -0,0 +1,68 @@
+CREATE DATABASE moviedb;
+USE moviedb;
+
+CREATE TABLE movies (
+    id VARCHAR(10) NOT NULL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    year INT NOT NULL,
+    director VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE stars (
+    id VARCHAR(10) NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    birthYear INT
+);
+
+CREATE TABLE stars_in_movies (
+    starId VARCHAR(10) NOT NULL,
+    movieId VARCHAR(10) NOT NULL,
+    FOREIGN KEY (starId) REFERENCES stars(id),
+    FOREIGN KEY (movieId) REFERENCES movies(id)
+);
+
+CREATE TABLE genres (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE genres_in_movies (
+    genreId INT NOT NULL,
+    movieId VARCHAR(10) NOT NULL,
+    FOREIGN KEY (genreId) REFERENCES genres(id),
+    FOREIGN KEY (movieId) REFERENCES movies(id)
+);
+
+CREATE TABLE creditcards (
+    id VARCHAR(20) NOT NULL PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    expiration DATE NOT NULL
+);
+
+CREATE TABLE customers (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    ccId VARCHAR(20) NOT NULL,
+    address VARCHAR(200) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    FOREIGN KEY (ccId) REFERENCES creditcards(id)
+);
+
+CREATE TABLE sales (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    customerId INT NOT NULL,
+    movieId VARCHAR(10) NOT NULL,
+    FOREIGN KEY (customerId) REFERENCES customers(id),
+    FOREIGN KEY (movieId) REFERENCES movies(id),
+    saleDate DATE NOT NULL
+);
+
+CREATE TABLE ratings (
+    movieId VARCHAR(10) NOT NULL,
+    rating FLOAT NOT NULL,
+    numVotes INT NOT NULL,
+    FOREIGN KEY (movieId) REFERENCES movies(id)
+);
