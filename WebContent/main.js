e87a7aec098339b4ed76339a83eb3ebdacb3bb45
Liam Chen“ <shuyic9@uci.edu>
2024-05-25 18:04:12 -0700

    added autocomplete functionality

--- WebContent/main.js
+++ WebContent/main.js
@@ -64,3 +64,53 @@ jQuery.ajax({
 $(document).ready(function () {
     handleTitleResult();
 });
+
+$(document).ready(function() {
+    // Cache to store autocomplete results
+    var cacheHash = {};
+
+    $('#autocomplete').autocomplete({
+        // Bind autocomplete to the input field with ID 'autocomplete'
+        lookup: function(query, doneCallback) {
+            if (query.length >= 3) { // Only perform lookup if query length is 3 or more characters
+                handleLookup(query, doneCallback);
+            }
+        },
+        onSelect: function(suggestion) {
+            // Define what happens when a suggestion is selected
+            handleSelectSuggestion(suggestion);
+        },
+        deferRequestBy: 300, // Delay before sending the request
+        minChars: 3 // Minimum characters before sending request
+    });
+
+    function handleLookup(query, doneCallback) {
+        // Check cache before sending AJAX request
+        if (query in cacheHash) {
+            console.log("using cache in frontend");
+            doneCallback({ suggestions: cacheHash[query] });
+        } else {
+            console.log("sending AJAX request to backend Java Servlet");
+            // Send AJAX request to the autocomplete servlet
+            jQuery.ajax({
+                method: "GET",
+                url: "api/autocomplete?query=" + escape(query),
+                success: function(data) {
+                    var jsonData = JSON.parse(data);
+                    cacheHash[query] = jsonData; // Cache the results
+                    doneCallback({ suggestions: jsonData });
+                },
+                error: function(errorData) {
+                    console.log("lookup ajax error");
+                    console.log(errorData);
+                }
+            });
+        }
+    }
+
+    function handleSelectSuggestion(suggestion) {
+        // Handle the selection of an autocomplete suggestion
+        console.log("you select " + suggestion["value"] + " with ID " + suggestion["data"]["ID"]);
+        window.location.href = 'single-movie.html?id=' + suggestion["data"]["ID"];
+    }
+});
